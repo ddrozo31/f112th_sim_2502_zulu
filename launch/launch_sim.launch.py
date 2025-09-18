21ecbdfe3322f9b2408ce524506051e8af52b38e
ddrozo <david.rozo31@eia.edu.co>
2025-09-17 19:31:06 -0500

    error

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='f112th_sim_2502_zulu' #<--- CHANGE ME
+    package_name='mr_odom_pkg' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
